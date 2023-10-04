2d7b0a1876e1033ccce77a760d3a5e51ae07069f
libertyskies <libertyemerson@outlook.com>
2023-10-04 10:03:06 +0100

    completing the patch article path and tests

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -6,6 +6,12 @@ exports.handleCustomErrors = (err, req, res, next) => {
   }
 };
 
+exports.handlePSQLErrors = (err, req, res, next) => {
+  if (err.code === "22003") {
+    res.status(400).send({ msg: "Value out of accepted range" });
+  }
+};
+
 exports.handleServerErrors = (err, req, res, next) => {
   console.log(err);
   res.status(500).send({ msg: "Internal Server Error" });
